70946f68c5d29810b99ae87c28fb43e3d53ddecc
Faisal <fasilshaikh@gmail.com>
2021-07-23 19:42:45 +0530

    change player logic added

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,14 @@
-let gameState = [
-    "X", "X", "X", 
-    "O", "", "X", 
-    "O", "O", "O"
+var gameState = [
+    "", "", "", 
+    "", "", "", 
+    "", "", ""
 ];
 
-let playerAction = "X"
+var playerAction = "X"
 
-let remainingTries = gameState.length
+var gameStop = false
+
+var remainingTries = gameState.length
 
 const winningConditions = [
     [0, 1, 2],
@@ -19,47 +21,41 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-function validateWinning(){
-    var gameResult = ""
-    for(var i=0; i<winningConditions.length; i++){
-        var counters = {X:0,O:0}
-        for(var j=0; j<winningConditions[i].length;j++){
-            var value = gameState[winningConditions[i][j]]
-            if(value){
-                counters[value]++
-            }
-        }
-        console.log(counters)
-        if(counters.X==3 || counters.O==3){
-            gameResult = value+ " Won the game"
-            break;
-        }
-    }
-
-    if(remainingTries==0 && gameResult==''){
-        gameResult = "Game Draw"
-    }
-
-    printResult(gameResult)
-}
-
-function printResult(result){    
+function printStatus(result){    
     if(result){
-        console.log(result)
-        document.getElementById('gameResult').innerHTML = result
+        document.getElementById('status').innerHTML = result
     }
 }
 
-function clearResult(){    
-    document.getElementById('gameResult').innerHTML = ""
+function clearStatus(){    
+    document.getElementById('status').innerHTML = ""
 }
 
-function chancePlayed(id){
-    clearResult()
-    if(gameState[id]!==""){
-        printResult("Please choose another block or restart the game.")
+function printError(result){    
+    if(result){
+        document.getElementById('error').innerHTML = result
+    }
+}
+
+function clearError(){    
+    document.getElementById('error').innerHTML = ""
+}
+
+function changePlayer(id){
+    if(!gameStop){
+        if(gameState[id]!==""){
+            printError("Please choose another block or restart the game.")
+        }else{
+            clearStatus()
+            clearError()
+            gameState[id] = playerAction;
+            document.getElementById(id).innerHTML = playerAction
+            playerAction = playerAction==='X' ? "O" : "X";
+            remainingTries--
+            printStatus(playerAction+"'s turn.")
+            validateWinning()
+        }
     }else{
-        console.log("in")
+        printError("Please restart the game.")
     }
 }
-validateWinning()
